1a694855aeff6647ff8b526d478817f4602020ba
Dev Unbelievable <devunbelievable@gmail.com>
2023-07-27 12:07:02 +0200

    test first part

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+
 //start project
 //my project give the random words of motivation
 /*how do it ?
- mon commit
+  ->a list of message in array
+  ->generate random number between 0 to array length
+  ->choice the content of number and show 
 */
+const messages = ["Succès", "Bravoure", "Strong", "Speed","Make It better"];
+function randomNumber(){
+    return Math.floor(Math.random()*messages.length);
+}
+
+// console.log(randomNumber(2));
+console.log(messages[randomNumber()]);
